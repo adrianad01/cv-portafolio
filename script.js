9a47697c3fdc8a438fcfa7b5535afcd5be1da814
Adrian Alcocer <jadrianaldo@outlook.com>
2024-02-26 18:46:18 -0600

    Update casi finalizado el sitio

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ btn.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
 
-alert("Página diseñada para dispositivos de 13'' en delante")
+alert("Página diseñada para dispositivos de 13'' en delante. El formulario aún no se encuentra validado, cualquier contacto es por medio del telefono/correo adjunto en el CV.");
